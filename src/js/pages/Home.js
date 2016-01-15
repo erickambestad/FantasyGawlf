8e0b6d57398255c5c72763cc78ce15c6536582ca
Eric Kambestad <erickambestad@yahoo.com>
2016-01-14 21:40:32 -0800

    updating home

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 
 import data from '../data.js'
 
@@ -183,11 +184,11 @@ class Home extends React.Component {
                   <div className="col-md-12">
                     <h3><strong>Schedule</strong></h3>
                     <ul style={{listStyle: 'none', paddingLeft: '0'}}>
-                      <li><strong>Quarter 1:</strong> Farmer's Insurance Open (1/28/16) - World Golf Championships-Dell Match Play (3/27/2016)</li>
-                      <li><strong>Quarter 2:</strong> Shell Houston Open (3/31/16) - Colonial National Invitation Tournament (5/29/2016)</li>
-                      <li><strong>Quarter 3:</strong> the Memorial Tournament presented by Nationwide (6/2/16) - RBC Canadian Open (7/24/2016)</li>
-                      <li><strong>Quarter 4:</strong> PGA Championship (7/28/16) - BMW Championship  (9/11/2016)</li>
-                      <li><strong>Championship:</strong> TOUR Championship by Coca-Cola (9/22/2016 - 9/25/2016)</li>
+                      <li><strong>Quarter 1:</strong> CareerBuilder Challenge (1/21/16) - Valspar Championship (3/13/2016)</li>
+                      <li><strong>Quarter 2:</strong> Arnold Palmer Invitational (3/17/16) - Wells Fargo Championship (5/8/2016)</li>
+                      <li><strong>Quarter 3:</strong> THE PLAYERS Championship (5/12/16) - World Golf Championships-Bridgestone Invitational (7/3/2016)</li>
+                      <li><strong>Quarter 4:</strong> The Greenbrier Classic (7/7/16) - Wyndham Championship (8/21/2016)</li>
+                      <li><strong>Championship:</strong> The Barclays (8/25/2016) - TOUR Championship by Coca-Cola (9/25/2016)</li>
                     </ul>
                   </div>
                   <div className="col-md-3">
@@ -198,6 +199,13 @@ class Home extends React.Component {
                   </div>
                   <div className="col-md-3">
                     <Leaderboard />
+                  </div>
+                  <div className="col-md-12">
+                    <hr />
+                  </div>
+                  <div className="col-md-12" style={{textAlign: 'center'}}>
+                    <h2>Your winnings: <span style={{color:'green',fontWeight:'bold'}}>$0</span></h2>
+                    <h6>As of {moment().format('MMMM Do YYYY')}</h6>
                   </div>
                 </div>
               )
@@ -211,7 +219,8 @@ class Home extends React.Component {
                         <ul>
                           <li>Select one golfer each week....each golfer can only be used once during the season and cannot be re-used if they miss the cut the first time you chose them</li>
                           <li>Dollars earned by your player each week is our points system--if they miss the cut, you earn $0 that week.  So bigger purse tournaments are more important than smaller ones</li>
-                          <li>The 2016 PGA Tour is split in to quarters and the winner from each quarter will face off in the final Tour Championship tournament. (Top players in each quarter will win prize money with the championship being the biggest pot)</li>
+                          <li>The 2016 PGA Tour is split in to quarters and the winner from each quarter will face off in the Championship (final 4 events of the year).</li>
+                          <li>Top players in each quarter will win prize money with the championship being the biggest pot</li>
                         </ul>
                       </div>
                       <div>
